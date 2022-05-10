9452bd3405e6048b99b30f9d0116b5d935396eaf
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2022-05-10 11:14:26 +0600

    header

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -58,10 +58,10 @@
 			<div class="top-menu">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-2">
-							<div id="colorlib-logo"><a href="/">Surfy</a></div>
+						<div class="col-md-4">
+							<div id="colorlib-logo"><a href="/">WEB TRAFFIC EXCHANGER</a></div>
 						</div>
-						<div class="col-md-10 text-right menu-1">
+						<div class="col-md-8 text-right menu-1">
                             <ul>
 								<li><a href="/">Home</a></li>
 								@if (Auth::guest())
